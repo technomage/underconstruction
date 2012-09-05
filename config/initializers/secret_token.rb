3186bd4b376baf2a95eda534dad200d4dde1cd5c
Michael Latta (TechnoMage) <mlatta@technomage.com>
2012-09-04 20:12:35 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Underconstruction::Application.config.secret_token = '0c48978e19c57007d12a43b5f04f0142fd45862ebd3c25095c5e5282a32edc8f599f55481d7b73d5ed4c2534e92eeb0d375042b3e8fa6cb1ee0315666f1d988c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Underconstruction::Application.config.session_store :cookie_store, key: '_underconstruction_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Underconstruction::Application.config.session_store :active_record_store
